c49ef1244d84cf1e7066482804fc65f11bb956e8
Serhii Seriogin <serhii.serohin@tacanslabs.com>
2023-09-22 10:26:10 -0400

    some fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Intro from "./components/intro";
 import Description from "./components/description";
+import Projects from "./components/projects";
 
 
 export default function Home() {
@@ -19,6 +20,7 @@ export default function Home() {
     <main>
       <Intro />
       <Description />
+      <Projects />
     </main>
   )
 }
