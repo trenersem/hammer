45f640e104f6fdf20495a7e00cbe56a9177e29b2
Serhii Seriogin <serhii.serohin@tacanslabs.com>
2023-09-28 10:52:31 -0400

    add contact

--- src/app/components/projects/index.tsx
+++ src/app/components/projects/index.tsx
@@ -102,7 +102,6 @@ export default function Projects() {
                         }
                     </div>
                 )}
-                
             </div>
             {detectMobile() &&  <Snapping />}
         </>
